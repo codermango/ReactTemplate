acdec0f95e1dee0538b04097dbf22caf352978f6
codermango <codermango@gmail.com>
2016-03-20 20:16:17 +0100

    Basic files

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: {
+        'index': './index.js'
+    },
+    output: {
+        path: './build',
+        filename: 'entry.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel',
+                query:{
+                    presets: ['es2015','react']
+                }
+            }
+        ]
+    }
+};
